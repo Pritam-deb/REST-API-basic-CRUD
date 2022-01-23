e4c2842e56ea13824a0df616231e40b157606ff3
Pritam <pritamdebnath297@gmail.com>
2022-01-23 13:09:03 +0530

    feat: sort out and delete user with ID

--- routes/users.js
+++ routes/users.js
@@ -8,24 +8,40 @@ let users = [
     firstName: "Josh",
     lastName: "Doe",
     age: 35,
+    id: 1,
   },
   {
     firstName: "Jean",
     lastName: "Poe",
     age: 38,
+    id: 2,
   },
 ];
 
+//SHOW ALL USERS
 router.get("/", (req, res) => {
   res.send(users);
 });
 
+//ADD NEW USERS
 router.post("/", (req, res) => {
-  // let user = req.body;
-  // const userId = uuidv4();
   const user = { ...req.body, id: uuidv4() };
   users.push(user);
   console.log(`${req.body} added to database`);
+  res.send(users);
+});
+
+//SHOW ONLY THE USER WITH GIVEN ID
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const result = users.find((user) => user.id == id);
+  res.send(result);
+});
+
+//DELETE THE USER
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
   res.send(users);
 });
 
