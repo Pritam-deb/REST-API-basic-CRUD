3c63e8a844ec737fa447f106da4915a68f233ef9
Pritam <pritamdebnath297@gmail.com>
2022-01-23 13:49:08 +0530

    feat: update user with ID

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
 import { Router } from "express";
+
+import { createUser } from "../controllers/users.js";
+
 const router = Router();
 
 let users = [
@@ -24,12 +27,7 @@ router.get("/", (req, res) => {
 });
 
 //ADD NEW USERS
-router.post("/", (req, res) => {
-  const user = { ...req.body, id: uuidv4() };
-  users.push(user);
-  console.log(`${req.body} added to database`);
-  res.send(users);
-});
+router.post("/", createUser);
 
 //SHOW ONLY THE USER WITH GIVEN ID
 router.get("/:id", (req, res) => {
@@ -42,6 +40,18 @@ router.get("/:id", (req, res) => {
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
   users = users.filter((user) => user.id !== id);
+  res.send(users);
+});
+
+//UPDATE A USER
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((user) => user.id == id);
+  const { firstName, lastName, age } = req.body;
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (age) user.age = age;
+
   res.send(users);
 });
 
